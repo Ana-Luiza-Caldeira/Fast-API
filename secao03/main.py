f2ce54cae3f4dc7bb056fc20339e7d8af9f8229f
AnaLuizaCaldeira <ana.caldeira@lymtech.com.br>
2023-12-11 12:36:44 -0300

    Add errors exception and create data

--- secao03/main.py
+++ secao03/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from typing import List, Optional
+from fastapi import FastAPI, HTTPException, status
+from models import Curso
 
 app = FastAPI()
 
@@ -15,7 +17,26 @@ cursos = {
     }
 }
 
+@app.get('/cursos')
+async def get_cursos():
+    return cursos
+
+@app.get('/cursos/{curso_id}')
+async def get_curso(curso_id: int):
+    try:
+        curso = cursos[curso_id]
+        return curso
+    except KeyError:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Curso não encontrado!')
+
+@app.post('/cursos', status_code=status.HTTP_201_CREATED)
+async def post_curso(curso: Curso):
+    next_id = len(cursos) +1
+    cursos[next_id] = curso
+    del curso.id
+    return curso
+
 if __name__ == '__main__':
     import uvicorn
 
-    uvicorn.run("main:app", host="0.0.0.0", port=8000)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
