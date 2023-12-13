08e44c0a81344e72814eb7858d702b93d3c508e7
AnaLuizaCaldeira <ana.caldeira@lymtech.com.br>
2023-12-13 11:41:17 -0300

    small adjusts

--- secao06/api/v1/endpoints/usuario.py
+++ secao06/api/v1/endpoints/usuario.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter, status, Depends, HTTPException, Response
 from fastapi.security import OAuth2PasswordRequestForm
 from fastapi.responses import JSONResponse
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.future import select
 from models.usuario_model import UsuarioModel
 from schemas.usuario_schema import UsuarioSchema, UsuarioSchamaArtigos, UsuarioSchemaBase, UsuarioSchemaCreate, UsuarioSchemaUp
@@ -16,9 +17,12 @@ router = APIRouter()
 async def post_signup(usuario: UsuarioSchemaCreate, db: AsyncSession = Depends(get_session)):
     novo_usuario: UsuarioModel = UsuarioModel(nome=usuario.nome, sobrenome=usuario.sobrenome, email=usuario.email, senha=gerar_hash_senha(usuario.senha), admin=usuario.admin)
     async with db as session:
-        session.add(novo_usuario)
-        await db.commit()
-        return novo_usuario
+        try:
+            session.add(novo_usuario)
+            await db.commit()
+            return novo_usuario
+        except IntegrityError:
+            raise HTTPException(status_code=status.HTTP_406_NOT_ACCEPTABLE, detail='Já existe um usuário com este email cadastrado')
 
 @router.get('/logado', response_model=List[UsuarioSchemaBase])
 async def get_logado(usuario_logado: UsuarioModel = Depends(get_current_user)):
